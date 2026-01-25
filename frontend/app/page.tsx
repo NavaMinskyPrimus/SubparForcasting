f90f4221abc1e868f47e1d2c360b6b230736637d
Nava Minsky-Primus <navaminskyprimus@gmail.com>
2026-01-25 16:58:18 -0500

    frontend shit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,15 @@
 import { signIn, signOut, auth } from "@/auth";
 import { redirect } from "next/navigation";
+import styles from "./landing.module.css";
 
 export default async function Home() {
   const session = await auth();
+ {/* Top image (not a banner overlay) */}
+      <img
+        src="/banner.png"
+        alt="Subparforecasting illustration"
+        className={styles.topImage}
+      />
 
   // If already logged in, send them straight to /instructions
   if (session?.user) {
